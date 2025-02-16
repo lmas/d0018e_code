d00caf5b420ca28e72fda227a5a85ea159a575d0
Pake <walpat-1@student.ltu.se>
2025-02-17 00:05:10 +0100

    Returned create_database.sql to normal, used wrong comment sign etc, will look at it tomorrow instead

--- schemas/create_database.sql
+++ schemas/create_database.sql
@@ -7,24 +7,9 @@ CREATE TABLE Products (
 	idProduct INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	Name VARCHAR(45) NOT NULL,
 	Price FLOAT NOT NULL,
-	/*
-	Speed INT NOT NULL,
-	Length FLOAG NOT NULL,
-	ImageFileName VARCHAR(45),
-	Connector1 INT NOT NULL,
-	Connector2 INT NOT NULL,
-	Color INT NOT NULL,
-	*/
 );
 
 -- Adds some example tuples to the table.
 INSERT INTO Products (Name, Price) VALUES
 ("Long USB-A", 100),
 ("Short USB-A", 50);
-
-/*
-INSERT INTO Products (Name, Price, Speed, Length, ImageFileName, Connector1, Connector2, Color) VALUES
-("Long USB-A", 100),
-("Short USB-A", 50);
-*/
-
