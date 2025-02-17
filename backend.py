c197caabfa7f1052be9262df452f68bc62def2cf
Pake <walpat-1@student.ltu.se>
2025-02-17 14:06:34 +0100

    First version of conversion tables/dicts WIP

--- backend.py
+++ backend.py
@@ -109,6 +109,16 @@ def close_db(exception=None):
 # Blue      = 5
 # Orange    = 6
 # Brown     = 7
+Colors = {
+    "Black":    0,
+    "White":    1,
+    "Red":      2,
+    "Yellow":   3,
+    "Green":    4,
+    "Blue":     5,
+    "Orange":   6,
+    "Brown":    7,
+}
 
 # -- Quick and dirty Connector table --
 # Type A 1.0-1.1    = 0
@@ -122,6 +132,21 @@ def close_db(exception=None):
 # Micro-AB          = 8
 # Micro-B           = 9
 # Type-C            = 10
+
+Connectors = {
+	"Type A":				0,
+	"Type A SuperSpeed":	1,
+	"Type B":				2,
+	"Type B SuperSpeed":	3,
+	"Mini-A":				4,
+	"Mini-AB":				5,
+	"Mini-B":				6,
+	"Micro-A":				7,
+    "Micro-AB":				8,
+    "Micro-B":				9,
+    "Type-C":				10,
+}
+
 ## --- END OF TABLES TO BE CONVERTED TO CONSTANTS ---
 
 ## --- FOR EASE OF USE WHILE ADDING A PRODUCT ---
@@ -130,9 +155,9 @@ def close_db(exception=None):
 
 # Values etc below taken from https://en.wikipedia.org/wiki/USB
 # -- Quick and dirty Standards & Types table - Only allow these when adding them products (No mismatch of standards/types)? --
-## USB 1.0          (Type-A 1.0, Type-B) # Only have 1.1 and assume 1.0 stopped manufacture when 1.1 was released?
+# USB 1.0			(Type-A 1.0, Type-B)
 # USB 1.1           (Type-A 1.1, Type-B, Mini-A, Mini-AB, Mini-B)
-## USB 2.0          (Typa-A 2.0, Type-B, Mini-A, Mini-AB, Mini-B) # Only have 2.0 Revised and assume 2.0 stopped manufacture when 2.0 Revised was released?
+# USB 2.0			(Typa-A 2.0, Type-B, Mini-A, Mini-AB, Mini-B)
 # USB 2.0 Revised   (Typa-A 2.0, Type-B, Mini-A, Mini-AB, Mini-B, Micro-A, Micro-AB, Micro-B, Type-C)
 # USB 3.0           (Type-A SuperSpeed, Type-B SuperSpeed, Micro-A SuperSpeed, Micro-AB SuperSpeed, Micro-B SuperSpeed, Type-C)
 # USB 3.1           (Type-A SuperSpeed, Type-B SuperSpeed, Micro-A SuperSpeed, Micro-AB SuperSpeed, Micro-B SuperSpeed, Type-C)
@@ -140,16 +165,42 @@ def close_db(exception=None):
 # USB4              (Type-C)
 # USB4 2.0          (Type-C)
 
+Standards_and_Types = {
+	"USB 1.0":			["Type A", "Type B"],
+	"USB 1.1":			["Type A", "Type B", "Micro A", "Micro AB", "Micro B"],
+    "USB 2.0":			["Type A", "Type B", "Mini A", "Mini AB", "Mini B"],
+    "USB 2.0 Revised":	["Type A", "Type B", "Mini A", "Mini AB", "Mini B", "Micro A", "Micro AB", "Micro B", "Type C"],
+    "USB 3.0":			["Type A", "Type B", "Mini A", "Mini AB", "Mini B", "Micro A", "Micro AB", "Micro B", "Type C"],
+    "USB 3.1":			["Type A", "Type B", "Mini A", "Mini AB", "Mini B", "Micro A", "Micro AB", "Micro B", "Type C"],
+    "USB 3.2":			["Type A", "Type B", "Mini A", "Mini AB", "Mini B", "Micro A", "Micro AB", "Micro B", "Type C"],
+    "USB4":				["Type C"],
+    "USB4 2.0":			["Type C"],
+}
+
 # -- Quick and dirty Speed table --
-## USB 1.0          1.5 Mbit/s, 12 Mbit/s
+# USB 1.0			1.5 Mbit/s, 12 Mbit/s
 # USB 1.1           1.5 Mbit/s, 12 Mbit/s
-## USB 2.0          480 Mbit/s
+# USB 2.0			480 Mbit/s
 # USB 2.0 Revised   480 Mbit/s
 # USB 3.0           5 Gbit/s (5000 Mbit/s)
 # USB 3.1           10 Gbit/s (10000 Mbit/s)
 # USB 3.2           20 Gbit/s (20000 Mbit/s)
 # USB4              40 Gbit/s (40000 Mbit/s)
 # USB4 2.0          80 Gbit/s (80000 Mbit/s)
+
+Speeds = {
+	"USB 1.0 Low-speed":	1.5,
+	"USB 1.1 Low-speed":	1.5,
+	"USB 1.0 High-speed":	12,
+	"USB 1.1 High-speed":	12,
+	"USB 2.0":				480,
+	"USB 2.0 Revised":		480,
+	"USB 3.0":				5000,
+	"USB 3.1":				10000,
+	"USB 3.1":				20000,
+	"USB4":					40000,
+	"USB4 2.0":				80000,
+}
 
 @app.route("/")
 def page_home():
